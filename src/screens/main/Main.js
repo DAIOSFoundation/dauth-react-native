d0d782dd4bcd2b6e7c09572e5ef563d47dd7cc38
송정엽 <jy_song@songjeong-yeob-ui-MacBook-Pro.local>
2020-07-02 18:03:51 +0900

    wip

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import {Text, View, SafeAreaView, Button} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import styled from 'styled-components';
 
 function Main() {
   return (
     <SafeAreaView>
+      <TitleWrap>
+        <TitleText>개인키 관리</TitleText>
+      </TitleWrap>
       <View>
-        <Text>Main</Text>
-        <Button title="Press me" />
+        <Text>정보관리</Text>
       </View>
     </SafeAreaView>
   );
 }
 
 export default Main;
+
+const TitleWrap = styled.View`
+  height: 50px;
+  justify-content: center;
+`;
+
+const TitleText = styled.Text`
+  width: 238px;
+  height: 24px;
+  padding-left: 20px;
+  font-size: 16px;
+  color: #48515a;
+`;
