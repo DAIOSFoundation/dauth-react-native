2f5e464fa6c59355d2aae019c9adab2956843a29
Kjintae <wlsxo2112@naver.com>
2020-07-16 12:46:30 +0900

    [Fix] conflict

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,6 +1,4 @@
 export {default as signup} from './signup/reducer';
-<<<<<<< HEAD
 export {default as mypage} from './mypage/reducer';
-=======
 export {default as login} from './login/reducer';
->>>>>>> 26790f30... [Add] login, signup 페이지 리덕스 구현
+
